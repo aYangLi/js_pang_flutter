4c63e4ada6ecdb929d79ded6707fb007ed2499f3
liChenYang <chenyang.li@dmall.com>
2019-10-24 16:21:45 +0800

    Text 组件

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,26 @@
-import 'package:flutter/material.dart'; // material 是一个UI库
+import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Text Widget',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
         body: Center(
-          child: Text('hello aYang'),
+          child: Text(
+            '喜欢学习的程序员；喜欢学习的程序员；喜欢学习的程序员；喜欢学习的程序员；喜欢学习的程序员；喜欢学习的程序员；喜欢学习的程序员；喜欢学习的程序员；喜欢学习的程序员；',
+            textAlign: TextAlign.center,
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Color.fromARGB(255, 255, 135, 125),
+              decoration: TextDecoration.underline,
+              decorationStyle: TextDecorationStyle.solid,
+            ),
+          ),
         ),
       ),
     );
