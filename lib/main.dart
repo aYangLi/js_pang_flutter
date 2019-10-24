a3a42f79dd5aeeb119e47874d1333533496d2b57
liChenYang <chenyang.li@dmall.com>
2019-10-24 18:09:44 +0800

    container 组件

--- lib/main.dart
+++ lib/main.dart
@@ -9,18 +9,26 @@ class MyApp extends StatelessWidget{
       title: 'Text Widget',
       home: Scaffold(
         body: Center(
-          child: Text(
-            '喜欢学习的程序员；喜欢学习的程序员；喜欢学习的程序员；喜欢学习的程序员；喜欢学习的程序员；喜欢学习的程序员；喜欢学习的程序员；喜欢学习的程序员；喜欢学习的程序员；',
-            textAlign: TextAlign.center,
-            maxLines: 2,
-            overflow: TextOverflow.ellipsis,
-            style: TextStyle(
-              fontSize: 20.0,
-              color: Color.fromARGB(255, 255, 135, 125),
-              decoration: TextDecoration.underline,
-              decorationStyle: TextDecorationStyle.solid,
+          child: Container(
+            child: Text(
+              'Hello aYang',
+              style: TextStyle(
+                fontSize: 30.0,
+              ),
             ),
-          ),
+            alignment:Alignment.topRight,
+            width: 500,
+            height: 400.0,
+            // color: Colors.lightBlue,
+            padding: const EdgeInsets.fromLTRB(10.0,30.0,0,30.0),
+            margin: const EdgeInsets.all(20.0),
+            decoration: BoxDecoration(
+              gradient: const LinearGradient(
+                colors: [Colors.lightBlue,Colors.greenAccent,Colors.purple]
+              ),
+              border: Border.all(width: 2.0,color: Colors.red)
+            ),
+          )
         ),
       ),
     );
