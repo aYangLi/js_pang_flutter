d08113dc4e8b79d087b6e6ba21808b65969d3f22
liChenYang <chenyang.li@dmall.com>
2019-10-24 15:34:42 +0800

    hello world

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart'; // material 是一个UI库
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+        ),
+        body: Center(
+          child: Text('hello aYang'),
+        ),
+      ),
+    );
+  }
+}
