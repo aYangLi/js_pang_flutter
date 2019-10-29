b0f9aa9bd7790007392188b7adbc8181867d354b
liChenYang <chenyang.li@dmall.com>
2019-10-29 15:20:16 +0800

    垂直方向布局

--- demo03/lib/main.dart
+++ demo03/lib/main.dart
@@ -11,26 +11,41 @@ class MyApp extends StatelessWidget{
         appBar: AppBar(
           title: Text('水平方向布局'),
         ),
-        body: Row(
+        // body: Row(
+        //   children: <Widget>[
+        //     Expanded(
+        //       child: RaisedButton(
+        //       onPressed: (){},
+        //       color: Colors.redAccent,
+        //       child: Text('红色按钮'),
+        //     )),
+        //      Expanded(
+        //       child: RaisedButton(
+        //       onPressed: (){},
+        //       color: Colors.orangeAccent,
+        //       child: Text('橘色按钮'),
+        //     )),
+        //      Expanded(
+        //       child: RaisedButton(
+        //       onPressed: (){},
+        //       color: Colors.green,
+        //       child: Text('绿色按钮'),
+        //     ))
+        //   ],
+        // ),
+        
+        // 垂直方向布局
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Expanded(
-              child: RaisedButton(
-              onPressed: (){},
-              color: Colors.redAccent,
-              child: Text('红色按钮'),
-            )),
-             Expanded(
-              child: RaisedButton(
-              onPressed: (){},
-              color: Colors.orangeAccent,
-              child: Text('橘色按钮'),
-            )),
-             Expanded(
-              child: RaisedButton(
-              onPressed: (){},
-              color: Colors.green,
-              child: Text('绿色按钮'),
-            ))
+            Text('我是 aYang，'),
+            Text('我是 aYang，'),
+            Expanded(child:Text('我是 aYang，阿阳')),
+            Text('我是 aYang，'),
+            Text('我是 aYang，'),
+            Text('我是 aYang，'),
+            Text('我是 aYang，')
           ],
         ),
       ),
